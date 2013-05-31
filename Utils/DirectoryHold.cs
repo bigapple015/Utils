9dde6b0adb7175ea0e28240d6b2050e88754197c
chlu <luchunminglu@gmail.com>
2013-05-31 00:38:50 -0700

    dir

---
+++ Utils/DirectoryHold.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+
+namespace Common.IO
+{
+    public class DirectoryHold : IDisposable
+    {
+        public DirectoryHold(string s)
+        {
+            m_strSavedDir = Directory.GetCurrentDirectory();
+            RetryDirectory.Default.SetCurrentDirectory(s);
+        }
+
+        void IDisposable.Dispose()
+        {
+            if (m_strSavedDir != null)
+            {
+                RetryDirectory.Default.SetCurrentDirectory(m_strSavedDir);
+                m_strSavedDir = null;
+            }
+        }
+
+        private string m_strSavedDir;
+    }
+}
